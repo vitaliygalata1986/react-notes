169bdfdfd25798cbfd4e3538d07f51f3528f9061
vitaliy <nertis44@gmail.com>
2023-11-05 17:40:43 +0200

    Использование JSX

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -2,8 +2,8 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  // return <div>Start project</div>;
-  return React.createElement('div', {}, 'Проект');
+  return <div>Start project</div>;
+  // return React.createElement('div', {}, 'Проект');
 }
 
 export default App;
