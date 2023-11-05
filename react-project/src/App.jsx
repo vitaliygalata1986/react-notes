81c3174916095fcc223aa17458d35a8d87850f08
vitaliy <nertis44@gmail.com>
2023-11-05 17:38:25 +0200

    Использование React.createElement вместо JSX

---
+++ react-project/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  // return <div>Start project</div>;
+  return React.createElement('div', {}, 'Проект');
+}
+
+export default App;
