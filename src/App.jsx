817cb0ac738e7b69a29aca5bd4c0b6de124e331d
vitaliy <nertis44@gmail.com>
2023-11-06 22:36:43 +0200

    Создали комопнент кнопки

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <>
       <h1>React Fragment</h1>
       <p>Lorem, ipsum.</p>
+      <Button />
     </>
   );
   // return React.createElement('div', {}, 'Проект');
