face03027f8e9dbbe28158da46064e1fb3708293
vitaliy <nertis44@gmail.com>
2023-11-09 23:31:36 +0200

    props/date

--- src/components/Button/JournalItem/JournalItem.jsx
+++ src/components/Button/JournalItem/JournalItem.jsx
@@ -1,15 +1,12 @@
 import './JournalItem.css';
 
-function JournalItem() {
-  const title = 'Подготовка к обновлению курсов';
-  const date = new Date();
-  const text =
-    'Горные походы открывают удивительные природные ландшафты, испытывают туристов физически и морально, дают возможность почувствовать себя первопроходцем.';
+function JournalItem({ title, date, text }) {
+  const formatedDate = new Intl.DateTimeFormat('uk-UA').format(date);
   return (
     <div className="journal-item">
       <h2 className="journal-item__header">{title}</h2>
       <div className="journal-item__body">
-        <div className="journal-item__date">{date.toString()}</div>
+        <div className="journal-item__date">{formatedDate}</div>
         <div className="journal-item__text">{text}</div>
       </div>
     </div>
